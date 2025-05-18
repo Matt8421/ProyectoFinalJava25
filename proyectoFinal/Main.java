2ff3912ae692b2b8bc08296c4b2e344d1d77c06f
Matt <matiaslucero21884@gmail.com>
2025-05-18 11:06:28 -0300

    Primer Commit

---
+++ proyectoFinal/Main.java
@@ -0,0 +1,10 @@
+package proyectoFinal;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hola Mundo desde Java");
+        sc.close();
+    }
+}
