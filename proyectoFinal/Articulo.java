dd9cf2241a80144bf85047081331cb4d97238acc
Matt <matiaslucero21884@gmail.com>
2025-05-18 12:16:52 -0300

    Clase Articulo

--- proyectoFinal/Articulo.java
+++ proyectoFinal/Articulo.java
@@ -1,5 +1,49 @@
 package proyectoFinal;
 
-public class Articulo {
 
+    //Clase base Articulo con encapsulamiento
+public class Articulo {
+    //Atributos privados para aplicar encapsulamiento
+    private int id;
+    private String nombre;
+    private double precio;
+
+    //Constructor para inicializar el objeto Articulo
+    public Articulo(int id, String nombre, double precio) {
+        this.id = id;                                   //"This" refiere al atributo del objeto
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    //Getter para el atributo id (Solo lectura)
+    public int getId() {
+        return id;
+    }
+    //No uso setter para el ID
+
+    //Getter para el atributo nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    //Setter para modificar el atributo nombre
+    public void setNombre(String nombreParametro) {
+        this.nombre = nombreParametro;
+    }
+
+    //Getter para el atributo precio
+    public double getPrecio() {
+        return precio;
+    }
+
+    //Setter para modificar el atributo precio
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    //Metodo para mostrar la informacion del articulo
+    public void mostrarArticulo() {
+        //Este metodoa puede ser sobrescrito por subclases (Polimorfismo)
+        System.out.println("ID: " + id + " | Nombre: " + nombre + " | Precio: $" + precio);
+    }
 }
